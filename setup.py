ca5e9075ec016dd20c6dfc08db9f0da582cfbbfc
jensjoh01 <johnnyj2439@gmail.com>
2017-10-23 14:14:32 -0700

    Added tests for creating and updating donors

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 
-# setup(
-#     entry_points={
-#     'console_scripts': [
-#                 'mail_room_madness = mail_room_madness:create_report'
-#             ]
-#         }
-#     )
+setup(
+    name='Mail Room Madness',
+    py_modules=['mail_room_madness'],
+    package_dir={'': 'src'},
+    install_requires=['tabulate'],
+    extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
+    entry_points={
+        'console_scripts': [
+            'mailroom = mail_room_madness:main'
+        ]
+    }
+)
